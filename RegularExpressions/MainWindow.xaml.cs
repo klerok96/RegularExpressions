99a5505489dbc533cbd931e56e76a3ef807460b4
klerok96 <andreyklerok@mail.ru>
2016-12-18 15:42:20 +0300

    Добавьте файлы проекта.

---
+++ RegularExpressions/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string pattern = @"^[1-9][0-9]*$";
+            string text = textBox.Text;
+
+            if (Regex.IsMatch(text, pattern))
+            {
+                MessageBox.Show("Верно");
+            }
+            else
+            {
+                MessageBox.Show("Не верно");
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                AboutBox1 f = new AboutBox1();
+                f.ShowDialog();
+            }
+        }
+    }
+}
