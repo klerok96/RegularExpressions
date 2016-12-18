04fcaca65e48c363b990ea480b85efbcd6cc75fd
klerok96 <andreyklerok@mail.ru>
2016-12-18 17:58:25 +0300

    Task 7

--- RegularExpressions/MainWindow.xaml.cs
+++ RegularExpressions/MainWindow.xaml.cs
@@ -28,13 +28,10 @@ namespace RegularExpressions
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            string pattern_city_street = @"^[A-Z][a-z]*$";
-            string pattern_house = @"^\d*$";
-            string text_c = textBox.Text;
-            string text_s = textBox1.Text;
-            string text_h = textBox2.Text;
+            string pattern = @"^<html>(\s*\w*\s*)*</html>$";
+            string text = textBox.Text;
 
-            if ((Regex.IsMatch(text_c, pattern_city_street) == true) && (Regex.IsMatch(text_s, pattern_city_street)) && (Regex.IsMatch(text_h, pattern_house)))
+            if (Regex.IsMatch(text, pattern))
             {
                 MessageBox.Show("Верно");
             }
